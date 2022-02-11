6badaecb743c9388d1db0f6298b397556d3d7a1d
conni <cbuschka@gmail.com>
2022-02-11 17:23:12 +0100

    Changed local storage format.

--- src/backend/initApp.js
+++ src/backend/initApp.js
@@ -1,8 +1,15 @@
 import {dispatcher} from "@cbuschka/flux";
+import {EdiReader} from "../ui/EdiReader";
 
 export const initApp = async () => {
     const ediFileJson = window.localStorage.getItem("openFiles.0");
     const ediFile = JSON.parse(ediFileJson);
 
-    return dispatcher.dispatch({type: "fileLoaded", data: ediFile})
+    try {
+        const ediReader = new EdiReader();
+        const ediFileData = ediReader.readFromString(ediFile.name, ediFile.text);
+        return dispatcher.dispatch({type: "fileLoaded", data: ediFileData})
+    } catch (e) {
+        console.error(e);
+    }
 }
--- src/backend/loadFiles.js
+++ src/backend/loadFiles.js
@@ -6,7 +6,7 @@ export const loadFiles = async (files) => {
     const ediReader = new EdiReader();
     const ediFile = ediReader.readFromString(files[0].name, files[0].data);
 
-    window.localStorage.setItem("openFiles.0", JSON.stringify(ediFile));
+    window.localStorage.setItem("openFiles.0", JSON.stringify({name: files[0].name, text: files[0].data}));
 
     return dispatcher.dispatch({type: "fileLoaded", data: ediFile})
 }
