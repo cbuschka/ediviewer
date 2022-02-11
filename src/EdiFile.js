ef058a102f63a4dcdc5fec7098ed4c227d4b94b3
conni <cbuschka@gmail.com>
2022-02-11 14:55:27 +0100

    Added basic indentation support.

--- src/EdiFile.js
+++ src/EdiFile.js
@@ -2,14 +2,32 @@ import React from 'react';
 import './EdiFile.css';
 import {EdiSegment} from "./EdiSegment";
 
+
+const indentMod = {
+    "UNH": 1,
+    "UNT": -1,
+};
+
+
 export class EdiFile extends React.Component {
 
     render() {
         const {data: {name, segments = []}} = this.props;
 
-        return <div><h2>{name}</h2>
-            {segments.map((segment, index) => {
-                return <EdiSegment key={index} data={segment}/>;
-            })}</div>;
+        return <div>
+            <h2>{name}</h2>
+            {this.renderSegments(segments)}
+        </div>;
+    }
+
+    renderSegments = (segments) => {
+        let indent = 0;
+        const content = [];
+        for (let i = 0; i < segments.length; ++i) {
+            const segment = segments[i];
+            content.push(<EdiSegment key={i} data={segment} indent={indent}/>);
+            indent = indent + (indentMod[segment.tag.value] || 0);
+        }
+        return content;
     }
 }
--- src/EdiSegment.js
+++ src/EdiSegment.js
@@ -5,9 +5,9 @@ import {EdiDataElement} from "./EdiDataElement";
 export class EdiSegment extends React.Component {
 
     render() {
-        const {data: {tag = "", dataElements = []}} = this.props;
+        const {data: {tag = "", dataElements = []}, indent = 0} = this.props;
 
-        return <div className={"EdiSegment EdiSegment__" + tag.value.trim()}>
+        return <div className={"EdiSegment EdiSegment__" + tag.value.trim() + " EdiSegment__Indent" + indent}>
             <span className="EdiSegment__Tag">{tag.value.trim()}</span>
             {dataElements.map((dataElement, index) => {
                 return <EdiDataElement key={index} data={dataElement}/>;
