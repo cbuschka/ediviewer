60b031eb72fe530dfd5cf25dddad8503174db05f
conni <cbuschka@gmail.com>
2022-02-11 09:58:12 +0100

    Added trimming tag value.

--- src/EdiSegment.js
+++ src/EdiSegment.js
@@ -7,8 +7,8 @@ export class EdiSegment extends React.Component {
     render() {
         const {data: {tag = "", dataElements = []}} = this.props;
 
-        return <div className={"EdiSegment EdiSegment__" + tag.value}>
-            <span className="EdiSegment__Tag">{tag.value}</span>
+        return <div className={"EdiSegment EdiSegment__" + tag.value.trim()}>
+            <span className="EdiSegment__Tag">{tag.value.trim()}</span>
             {dataElements.map((dataElement, index) => {
                 return <EdiDataElement key={index} data={dataElement}/>;
             })}
